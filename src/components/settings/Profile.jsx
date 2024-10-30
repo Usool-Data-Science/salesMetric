c7d0acc07b3020f0776eb7b3d97b3036e9db9722
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-10-30 17:53:16 +0100

    Update

--- src/components/settings/Profile.jsx
+++ src/components/settings/Profile.jsx
@@ -13,8 +13,8 @@ const Profile = () => {
 				/>
 
 				<div>
-					<h3 className='text-lg font-semibold text-gray-100'>John Doe</h3>
-					<p className='text-gray-400'>john.doe@example.com</p>
+					<h3 className='text-lg font-semibold text-gray-100'>Adeshina Ibrahim</h3>
+					<p className='text-gray-400'>adeshinaibrahim10@gmail.com</p>
 				</div>
 			</div>
 
